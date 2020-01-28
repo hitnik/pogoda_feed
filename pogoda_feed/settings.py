e172fb6552fc0cb704c36b18fa017ebe41643e3a
hitnik <hitnik@gmail.com>
2020-01-28 20:04:31 +0300

    add db settings(environment support)

--- pogoda_feed/settings.py
+++ pogoda_feed/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v#&&)%n+%3!bbikpvhfm6gdjtdx9=)t15fp=$^nht@rgqs(gq3'
+SECRET_KEY = os.getenv('SECRET_KEY', 'foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 1)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
