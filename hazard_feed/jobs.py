bd59303ddda3e62ac514eb1b696dad7dd78e89ed
hitnik <hitnik@gmail.com>
2020-01-29 07:45:29 +0300

    add jobs.py

--- hazard_feed/jobs.py
+++ hazard_feed/jobs.py
@@ -1,108 +1,24 @@
 from django_rq import job
-from django.core import mail
-from django.core.mail import EmailMessage, EmailMultiAlternatives
-from forumTopics.models import Employees, Topics, ForumUsers
+from .config import WEATHER_FEED_URL
+import asyncio
 import datetime
-from django.conf import settings
-from django.template import Context, loader
-from babel.dates import format_date
-import logging
-import logging.config
-import pdfkit
+from hazard_feed.utils import (
+    parse_weather_feeds, put_feed_to_db,
+    make_weather_hazard_message, send_weather_mail,
+    get_weather_recipients
+    )
 
-DICTLOGCONFIG={
-"version":1,
-        "handlers":{
-            "fileHandler":{
-                "class":"logging.FileHandler",
-                "formatter":"myFormatter",
-                "filename":"/home/odo/djhelpdesk/forumTopics/djhelpdeskclearDB.log"
-            }
-        },
-        "loggers":{
-            "clearDB":{
-                "handlers":["fileHandler"],
-                "level":"INFO",
-            }
-        },
-        "formatters":{
-            "myFormatter":{
-                "format":"[%(asctime)s] - %(levelname)s - %(message)s"
-            }
-        }
-}
-
-@job
-def sendmail():
-    today = datetime.date.today()
-    oneday = datetime.timedelta(days=1)
-    yesterday = today - oneday
-    dateString = format_date(yesterday, format='long', locale='ru')
-    URLStart = settings.DJANGO_HOST
-    try:
-        pdfkit.from_url('http://' + URLStart + '/forums/plain', '/home/odo/djhelpdesk/forums.pdf')
-    except IOError as e:
-        pass
-    url = 'http://' + URLStart + '/forums/'
-
-    employees = Employees.objects.filter(isActive=True)
-    topicsCount = Topics.objects.filter(datePost__date=yesterday).count()
-    topicsCountLastDigit = topicsCount % 10
-    review = 'отзывов'
-    left = 'оставлено'
-    if topicsCountLastDigit == 1:
-        review = "отзыв"
-        left = 'оставлен'
-    elif topicsCountLastDigit in [2, 3, 4]:
-        review = 'отзыва'
-
-    with mail.get_connection() as connection:
-        for employee in employees:
-            if employee.patronymic:
-                fullName = employee.firstName + ' ' + employee.patronymic
-            else:
-                fullName = employee.firstName
-            t = loader.get_template('forumTopics/email.html')
-            context = {'fullName': fullName, 'dateString': dateString,
-                       'url': url, 'topicsCount': topicsCount, 'review': review, 'left': left}
-            html = t.render(context)
-            text = 'Здравствуйте, ' + fullName + '. За ' + dateString + \
-                   ' ' + left + ' ' + str(topicsCount) + ' ' + review + \
-                   ' о Белтелеком. Информация в прикрепленном файле или по ссылке: ' + url
-            subject = 'Отзывы о Белтелеком за ' + yesterday.strftime('%d.%m.%Y')
-            msg = EmailMultiAlternatives(subject, text, 'ОДО <omc@main.beltelecom.by>', [employee.email])
-            msg.attach_file('/home/odo/djhelpdesk/forums.pdf')
-            msg.attach_alternative(html, "text/html")
-            msg.send()
-
-@job
-def deleteOldTopics():
-    logging.config.dictConfig(DICTLOGCONFIG)
-    logger = logging.getLogger("clearDB")
-    today = datetime.date.today()
-    yearAgo = today
-    yearAgo = yearAgo.replace(year=yearAgo.year - 1)
-    deleteCandidates = Topics.objects.filter(datePost__datetz_lt=yearAgo).defer('id', 'user_id')
-    logger.info("%d candidates for deletion" % (len(deleteCandidates)))
-    count = 0;
-    for topic in deleteCandidates:
-        userCount = Topics.objects.filter(user__id=topic.user_id).count()
-        if userCount == 1:
-            count += 1
-            ForumUsers.objects.filter(id=topic.user_id).delete()
-        else:
-            Topics.objects.filter(id=topic.id).delete()
-    logger.info("delete %d topics, %d forumUsers" % (len(deleteCandidates), count))
-
-    fileName = DICTLOGCONFIG['handlers']['fileHandler']['filename']
-    linesToWrite = []
-    countLines = 0
-    with open(fileName, 'r') as f:
-        lines = f.readlines()
-        for line in list(reversed(lines)):
-            linesToWrite.append(line)
-            countLines += 1
-            if countLines > 100: break
-    with open(fileName, 'w') as f:
-        for line in list(reversed(linesToWrite)):
-            f.write(line)
+@job()
+def parse_and_send_feed():
+    feeds = parse_weather_feeds(WEATHER_FEED_URL)
+    recipients = get_weather_recipients()
+    msg = make_weather_hazard_message(feeds[0])
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(send_weather_mail(msg, recipients))
+    # for feed in feeds:
+    #     put_result = put_feed_to_db(feed)
+    #     if put_result and \
+    #             (
+    #                     feed.date.date() == feed.date_modified.date() or
+    #                     feed.is_newer_that(datetime.timedelta(hours=1))
+    #             ):
