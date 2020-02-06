7fdc15ec5665b403d5884bce7acad3e8b3584de7
hitnik <hitnik@gmail.com>
2020-02-06 20:18:37 +0300

    install cryptography

--- hazard_feed/apps.py
+++ hazard_feed/apps.py
@@ -42,7 +42,7 @@ class HazardFeedConfig(AppConfig):
             scheduler = django_rq.get_scheduler('default')
             scheduler.schedule(scheduled_time=datetime.datetime.utcnow() + datetime.timedelta(seconds=5),
                                func=jobs.parse_feeds,
-                               interval=6000
+                               interval=600
                                )
         except ConnectionError:
             pass
