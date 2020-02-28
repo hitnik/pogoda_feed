dbc4a4f2bf458ab35ae6658403234c902a6c2c26
hitnik <hitnik@gmail.com>
2020-02-28 06:38:39 +0300

    fix

--- hazard_feed/signals.py
+++ hazard_feed/signals.py
@@ -10,9 +10,8 @@ def send_hazard_feed_notification(sender, instance, created, **kwargs):
                 or instance.is_newer_that(datetime.timedelta(hours=1))
             ):
         try:
-            # queue = django_rq.get_queue()
-            # queue.enqueue(send_notification, instance)
-            send_notification(instance)
+            queue = django_rq.get_queue()
+            queue.enqueue(send_notification, instance)
             instance.date_send_set()
             instance.is_sent = True
             instance.save()
