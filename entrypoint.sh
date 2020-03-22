74109dc34d1e00e9ec92eb4d061b34df516b2038
hitnik <hinik@gmail.com>
2020-03-22 19:15:28 +0300

    migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/hitnik/pogoda_feed.git
+git init .
+git remote add origin https://github.com/hitnik/pogoda_feed.git
+git pull origin master
 
 
 python manage.py migrate
