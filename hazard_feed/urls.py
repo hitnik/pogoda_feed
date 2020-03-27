8014f25fc8736c069ca4b678185b8c7bf39f443b
hitnik <hitnik@gmail.com>
2020-03-27 07:56:19 +0300

    start to write NewsletterUnsibscribeAPIVIEW

--- hazard_feed/urls.py
+++ hazard_feed/urls.py
@@ -5,7 +5,9 @@ app_name = 'hazard_feed'
 
 urlpatterns = [
     path('v1/subscribe_newsletter', NewsletterSubscribeAPIView.as_view(), name='subscribe_newsletter'),
+    path('v1/unsubscribe_newsletter', NewsletterUnsibscribeAPIVIEW.as_view(), name='unsubscribe_newsletter'),
     path('v1/jobs', ScheduledJobsView.as_view(), name='jobs'),
     path('v1/activate', SubscribeActivationAPIView.as_view(), name='activate_subscribe'),
+    path('v1/deaactivate', SubscribeDeactivationAPIView.as_view(), name='deactivate_subscribe'),
 ]
 
