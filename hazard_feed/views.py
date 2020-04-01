82b5f0f70b1da5b19994117c079361d770b60d8a
hitnik <hinik@gmail.com>
2020-04-01 21:35:59 +0300

    WeatherRecipientsMailSerializer raises ValidationError with code 404 when email not exist

--- hazard_feed/views.py
+++ hazard_feed/views.py
@@ -77,6 +77,7 @@ class NewsletterUnsubscribeAPIVIEW(generics.GenericAPIView):
         return Response(status=status.HTTP_200_OK)
 
     def handle_exception(self, exc):
+        print(exc)
         if isinstance(exc, ValidationError404):
             print(exc.status_code)
 
