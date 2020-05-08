79f1e3919a1c49616f39cf957c89137c6b23b4a2
hitnik <hitnik@gmail.com>
2020-05-08 23:40:25 +0300

    add api schema

--- hazard_feed/urls.py
+++ hazard_feed/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import *
+from rest_framework.schemas import get_schema_view
 
 app_name = 'hazard_feed'
+
+schema_view = get_schema_view(
+    title='Weather hazard feeds API',
+    version= '1.0.0',
+)
 
 urlpatterns = [
     path('v1/subscribe_newsletter', NewsletterSubscribeAPIView.as_view(), name='subscribe_newsletter'),
@@ -9,5 +15,7 @@ urlpatterns = [
     path('v1/jobs', ScheduledJobsView.as_view(), name='jobs'),
     path('v1/activate', SubscribeActivationAPIView.as_view(), name='activate_subscribe'),
     path('v1/deactivate', SubscribeDeactivationAPIView.as_view(), name='deactivate_subscribe'),
+    path('openapi', schema_view, name='openapi-schema'),
+
 ]
 
