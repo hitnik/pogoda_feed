e4863f1d6046257d530f75df152a54f76a4f1bcd
hitnik <hitnik@gmail.com>
2020-07-29 07:54:54 +0300

    return expiration datetime as seconds in json response

--- hazard_feed/views.py
+++ hazard_feed/views.py
@@ -45,7 +45,7 @@ class NewsletterSubscribeAPIView(generics.GenericAPIView):
         code = EmailActivationCode.objects.create(target=recipient, is_activate=True)
         token = jwt.encode({'id': code.id.__str__(), 'exp': code.date_expiration},
                            settings.SECRET_KEY, algorithm='HS256').decode('utf-8')
-        data = {'expires': int(time.mktime(code.date_expiration.timetuple())),
+        data = {'expires': int(time.mktime(code.date_expiration.timetuple()*1000)),
                 'token': token,
                 'code_confirm': reverse_lazy('hazard_feed:code_validate')
                 }
