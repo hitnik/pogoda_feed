fbfc99d25c8b4697ec75ed888d4da37bf1a5953d
hitnik <hitnik@gmail.com>
2020-12-18 14:51:48 +0300

    remove guincorn and uvicorn from requirements.txt and add daphne as asgi server

--- pogoda_feed/asgi.py
+++ pogoda_feed/asgi.py
@@ -7,18 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
+from .wsgi import *
 import os
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import hazard_feed.routing
-from .wsgi import *
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pogoda_feed.settings')
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
             hazard_feed.routing.websocket_urlpatterns
--- pogoda_feed/settings.py
+++ pogoda_feed/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -31,7 +33,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'hazard_feed',
+    'channels',
 ]
 
 MIDDLEWARE = [
