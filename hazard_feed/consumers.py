34779c62d4d603e6a8c2c2db9a5883999750ae34
hitnik <hitnik@gmail.com>
2020-12-20 08:52:53 +0300

    websocket interaction scheme

--- hazard_feed/consumers.py
+++ hazard_feed/consumers.py
@@ -6,7 +6,7 @@ class WeatherJsonConsumer(AsyncJsonWebsocketConsumer):
     group_name = 'weather'
     response = {'response': 'ok'}
     ping_response = dict(response, payload='pong')
-
+    empty_response = dict(response, payload='empty')
 
     async def connect(self):
 
@@ -20,7 +20,7 @@ class WeatherJsonConsumer(AsyncJsonWebsocketConsumer):
         await self.channel_layer.send(self.channel_name,
             {
               'type': 'weather.notify',
-              'content': self.ping_response
+              'content': self.response
             }
         )
 
@@ -31,8 +31,42 @@ class WeatherJsonConsumer(AsyncJsonWebsocketConsumer):
             self.channel_name
         )
 
+    async def receive_json(self, content):
+        # receive messages from socket
+
+        if content['payload'] == 'feeds':
+            await self.send_weather_response()
+            return
+        await self.send_ping_response()
+
+
+
+    async def send_ping_response(self):
+        # send pong message to socket
+        await self.channel_layer.send(self.channel_name,
+              {
+                  'type': 'weather.notify',
+                  'content': self.ping_response
+              }
+              )
+
+    async def send_weather_response(self):
+        # send weather message to socket
+
+        feeds = await get_actial_hazard_feeds()
+        if feeds:
+            response = dict(self.response, **feeds)
+        else:
+            response = self.empty_response
+
+        await self.channel_layer.send(self.channel_name,
+              {
+                  'type': 'weather.notify',
+                  'content': response
+              }
+              )
+
     async def weather_notify(self, event):
         content = event['content']
-
         # Send message to WebSocket
         await self.send_json(content=content)
